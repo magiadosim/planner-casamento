6c22ad86ede58af5cee3fe826d219b7de61cee3c
magiadosim <amagiadosim2026@gmail.com>
2026-09-24 17:36:59 -0300

    config: preparar hCaptcha para o cadastro

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 window.PLANNER_CONFIG = Object.freeze({
   supabaseUrl: 'https://leuudsaoryeigobyrtal.supabase.co',
-  supabasePublishableKey: 'sb_publishable_lYSEbr4ak5G5bTS0H38CeA_m1iudzBb'
+  supabasePublishableKey: 'sb_publishable_lYSEbr4ak5G5bTS0H38CeA_m1iudzBb',
+  // hCaptcha Site Key. Preencha após criar o site no hCaptcha e ativar CAPTCHA no Supabase Auth.
+  captchaSiteKey: ''
 });
